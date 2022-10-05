39f20689c1485d44dd507a95696d5b131c768a79
Cecilia Janco <cecielias003@gmail.com>
2022-10-05 09:32:36 -0300

    model experiencia

---
+++ ceci/src/main/java/com/portfolio/ceci/dto/DtoExperiencia.java
@@ -0,0 +1,29 @@
+package com.portfolio.ceci.dto;
+
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DtoExperiencia {
+
+    @NotBlank
+    private String nombreExp;
+    @NotBlank
+    private String descripcionExp;
+    @NotBlank
+    private String fechaExp;
+    @NotBlank
+    private String imagenExp;
+
+    public DtoExperiencia() {
+    }
+
+    public DtoExperiencia(String nombreExp, String descripcionExp, String fechaExp, String imagenExp) {
+        this.nombreExp = nombreExp;
+        this.descripcionExp = descripcionExp;
+        this.fechaExp = fechaExp;
+        this.imagenExp = imagenExp;
+    }
+}
---
+++ ceci/src/main/java/com/portfolio/ceci/repository/IExperienciaRepository.java
@@ -0,0 +1,14 @@
+package com.portfolio.ceci.repository;
+
+import com.portfolio.ceci.entity.Experiencia;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IExperienciaRepository extends JpaRepository<Experiencia, Integer> {
+
+    public Optional<Experiencia> findByNombreExp(String nombreExp);
+
+    public boolean existsByNombreExp(String nombreExp);
+}
